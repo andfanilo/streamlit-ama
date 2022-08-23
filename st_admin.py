3dfe9a50047b2ac2f7c7084dc77ecdff4bedf4a3
Fanilo Andrianasolo <andfanilo@gmail.com>
2022-08-23 12:22:38 +0200

    Update Admin page that displays all messages

--- st_admin.py
+++ st_admin.py
@@ -1,37 +1,67 @@
+import json
 import streamlit as st
 
 from google.cloud import firestore
-from google.cloud.firestore import Client
+from google.oauth2 import service_account
+from streamlit_elements import elements
+from streamlit_elements import html
+from streamlit_elements import mui
 
 
 @st.experimental_singleton
 def get_db():
-    db = firestore.Client.from_service_account_json("key.json")
+    key_dict = json.loads(st.secrets["firestore"]["textkey"])
+    creds = service_account.Credentials.from_service_account_info(key_dict)
+    db = firestore.Client(credentials=creds, project="streamlit-ama-a2065")
     return db
 
 
-def post_message(db: Client, input_name, input_message):
-    payload = {
-        "name": input_name,
-        "message": input_message,
-        "answer": None,
-    }
-    doc_ref = db.collection("messages").document()
-
-    doc_ref.set(payload)
-    return
+@st.experimental_memo
+def get_all_messages():
+    db = get_db()
+    all_messages = db.collection("messages").stream()
+    return [m.to_dict() for m in all_messages]
 
 
 def main():
-    st.title("Admin page")
-    db = get_db()
+    if "page" not in st.session_state:
+        st.session_state.page = 0
 
-    all_messages = db.collection("messages")
+    def handle_change(_, page):
+        st.session_state.page = page - 1
 
-    for doc in all_messages.stream():
-        st.json(doc.to_dict())
+    all_messages = get_all_messages()
+
+    st.title(":balloon: Fanilo's AMA | Admin dashboard")
+
+    with elements("main"):
+        mui.Divider()
+
+        message = all_messages[st.session_state.page]["message"]
+        author = all_messages[st.session_state.page]["name"]
+        author = author if author != "" else "anonymous"
+
+        with mui.Box(
+            sx={
+                "my": 3,
+                "p": 3,
+                "bgcolor": "background.paper",
+                "boxShadow": 1,
+                "borderRadius": 2,
+            }
+        ):
+            mui.Typography(message)
+            mui.Typography(
+                f"by {author}",
+                typography="subtitle2",
+                fontWeight="light",
+                textAlign="right",
+                sx={"mt": 3},
+            )
+
+        mui.Pagination(count=len(all_messages), defaultPage=1, onChange=handle_change)
 
 
 if __name__ == "__main__":
-    st.set_page_config(page_title="Fanilo's AMA Admin", page_icon=":glasses:")
+    st.set_page_config(page_title="Fanilo's AMA | Admin", page_icon=":balloon:")
     main()
