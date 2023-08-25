d7089f85f98d328388e80ed30e8f90cb47085635
Fanilo Andrianasolo <andfanilo@users.noreply.github.com>
2023-08-25 16:28:29 +0200

    Remove streamlit elements

--- st_admin.py
+++ st_admin.py
@@ -3,9 +3,6 @@ import streamlit as st
 
 from google.cloud import firestore
 from google.oauth2 import service_account
-from streamlit_elements import elements
-from streamlit_elements import html
-from streamlit_elements import mui
 
 
 @st.cache_resource
@@ -24,48 +21,15 @@ def get_all_messages():
 
 
 def main():
-    if "page" not in st.session_state:
-        st.session_state.page = 0
-
     with st.sidebar:
         entered_pwd = st.text_input("Enter Password", type="password")
     if entered_pwd != st.secrets["admin_password"]:
         st.error("Enter correct password", icon="🚨")
         st.stop()
 
-    def handle_change(_, page):
-        st.session_state.page = page - 1
-
     all_messages = get_all_messages()
 
     st.title(":balloon: Fanilo's AMA | Admin dashboard")
-
-    with elements("main"):
-        mui.Divider()
-
-        message = all_messages[st.session_state.page]["message"]
-        author = all_messages[st.session_state.page]["name"]
-        author = author if author != "" else "anonymous"
-
-        with mui.Box(
-            sx={
-                "my": 3,
-                "p": 3,
-                "bgcolor": "background.paper",
-                "boxShadow": 1,
-                "borderRadius": 2,
-            }
-        ):
-            mui.Typography(message)
-            mui.Typography(
-                f"by {author}",
-                typography="subtitle2",
-                fontWeight="light",
-                textAlign="right",
-                sx={"mt": 3},
-            )
-
-        mui.Pagination(count=len(all_messages), defaultPage=1, onChange=handle_change)
 
     with st.expander("Show all questions"):
         st.write(all_messages)
